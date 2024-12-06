eb98f841a08e58ee5250fa3d8ddf39ab33555462
Admin <209695793@qq.com>
2024-12-06 20:42:31 +0800

    添加yargs的命令用例

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env node
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const { readFile } = require("node:fs/promises");
@@ -38,14 +38,31 @@ async function main() {
 
 // main();
 
-const _yargs = require("yargs");
-const argv = _yargs
-  .command("get", "make a get HTTP request", {
-    url: {
-      alias: "u",
-      default: "http://yargs.js.org/",
-    },
-  })
-  .help()
-  .parse();
-console.log("argv: ", argv);
+function command() {
+  const _yargs = require("yargs");
+  const argv = _yargs
+    .command("get", "make a get HTTP request", {
+      url: {
+        alias: "u",
+        default: "http://yargs.js.org/",
+      },
+    })
+    .help()
+    .parse();
+  console.log("argv: ", argv);
+}
+
+// command();
+const fs = require("fs");
+
+function config() {
+  var argv = require("yargs/yargs")(process.argv.slice(2))
+    .config("settings", function (configPath) {
+      console.log("configPath: ", configPath);
+      return JSON.parse(fs.readFileSync(configPath, "utf-8"));
+    })
+    .parse();
+  console.log("argv: ", argv);
+}
+
+config();
